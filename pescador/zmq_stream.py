3336922fc2f3c1dd14dec37d51ef84e85cdba0f7
Brian McFee <brian.mcfee@nyu.edu>
2015-10-18 14:27:38 -0400

    switched zmq workre terminate to a semaphore+join

--- pescador/zmq_stream.py
+++ pescador/zmq_stream.py
@@ -73,7 +73,7 @@ def zmq_recv_batch(socket, flags=0, copy=True, track=False):
     return results
 
 
-def zmq_worker(port, streamer, copy=False, max_batches=None):
+def zmq_worker(port, streamer, terminate, copy=False, max_batches=None):
 
     context = zmq.Context()
     socket = context.socket(zmq.PAIR)
@@ -82,6 +82,8 @@ def zmq_worker(port, streamer, copy=False, max_batches=None):
     try:
         # Build the stream
         for batch in streamer.generate(max_batches=max_batches):
+            if terminate.is_set():
+                break
             zmq_send_batch(socket, batch, copy=copy)
 
     finally:
@@ -137,9 +139,10 @@ def zmq_stream(streamer, max_batches=None,
                                           min_port=min_port,
                                           max_port=max_port,
                                           max_tries=max_tries)
+        terminate = mp.Event()
 
         worker = mp.Process(target=SafeFunction(zmq_worker),
-                            args=[port, streamer],
+                            args=[port, streamer, terminate],
                             kwargs=dict(copy=copy,
                                         max_batches=max_batches))
 
@@ -157,5 +160,7 @@ def zmq_stream(streamer, max_batches=None,
         six.reraise(*sys.exc_info())
 
     finally:
-        worker.terminate()
+        #worker.terminate()
+        terminate.set()
+        worker.join()
         context.destroy()
