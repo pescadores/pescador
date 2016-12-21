c088b8d91c2147d8c24f5f32958f61e8ef841722
Brian McFee <brian.mcfee@nyu.edu>
2016-12-21 15:20:01 -0500

    removed all live code assertions

--- pescador/mux.py
+++ pescador/mux.py
@@ -95,8 +95,14 @@ class Mux(core.Streamer):
 
         self.pool_weights = np.atleast_1d(self.pool_weights)
 
-        assert len(self.pool_weights) == len(self.seed_pool)
-        assert (self.pool_weights > 0.0).any()
+        if len(self.pool_weights) != len(self.seed_pool):
+            raise PescadorError('pool_weights must be the same '
+                                'length as seed_pool')
+
+        if not (self.pool_weights > 0.0).any():
+            raise PescadorError('pool_weights must contain at least '
+                                'one positive value')
+
         self.pool_weights /= np.sum(self.pool_weights)
 
     def activate(self):
@@ -195,9 +201,16 @@ class Mux(core.Streamer):
         Parameters
         ----------
         idx : int
+            The seed index to replace
         '''
-        assert (len(self.seed_pool) == len(self.pool_weights) ==
-                len(self.seed_distribution))
+        if len(self.seed_pool) != len(self.pool_weights):
+            raise PescadorError('seed_pool must have the same '
+                                'length as pool_weights')
+
+        if len(self.seed_pool) != len(self.seed_distribution):
+            raise PescadorError('seed_pool must have the same '
+                                'length as seed_distribution')
+
         # instantiate
         if self.lam is not None:
             n_stream = 1 + np.random.poisson(lam=self.lam)
