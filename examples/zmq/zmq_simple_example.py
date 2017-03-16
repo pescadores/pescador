248ab56bd2c421cd28214e42880c858f08b33eea
Christopher Jacoby <christopher.jacoby@gmail.com>
2017-03-15 22:00:20 -0700

    initial zmq example

--- examples/zmq/zmq_simple_example.py
+++ examples/zmq/zmq_simple_example.py
@@ -9,12 +9,20 @@ import numpy as np
 import pescador
 
 
-def npy_generator(filepath):
-    pass
+def batch_gen():
+    """
+    Returns
+    -------
+    batch_dict
+        A batch which looks like it might come from some
+        machine learning problem, with X as Features, and Y as targets.
+    """
+    yield dict(X=np.random.random((1, 10)),
+               Y=np.random.randint(10))
 
 
 # Construct a streamer
-s = pescador.streamer(npy_generator)
+s = pescador.Streamer(batch_gen)
 
 # Wrap teh streamer in a ZMQ streamer
 zs = pescador.ZMQStreamer(s)
@@ -26,4 +34,17 @@ for batch in zs.generate(max_batches=10):
 
     # Train your network, etc.
 
-print("")
+print("Generated {} batches from ZMQ".format(batch_count))
+
+# Now, you could also wrap the ZMQStreamer in a BufferedStreamer, like so:
+buffer_size = 10
+buffered_zmq = pescador.BufferedStreamer(zs, buffer_size)
+
+# Get batches from the stream as you would normally.
+batch_count = 0
+for batch in zs.generate(max_batches=10):
+    batch_count += len(batch)
+
+    print("Received {} batches.")
+
+print("Generated {} batches from Buffered ZMQ Streamer".format(batch_count))
