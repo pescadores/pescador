3f8edb80ec501ba001f8ab75ce123cf7fafdb614
Christopher Jacoby <christopher.jacoby@gmail.com>
2017-03-16 22:50:08 -0700

    further refinements to keras example

--- examples/keras_example.py
+++ examples/keras_example.py
@@ -12,79 +12,69 @@ An example of how to use Pescador with Keras.
 
 from __future__ import print_function
 import numpy as np
-
+import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras.utils import np_utils
 from keras import backend as K
 
 import pescador
 
 batch_size = 128
-nb_classes = 10
-num_epochs = 6
+num_classes = 10
+epochs = 12
 
 # input image dimensions
 img_rows, img_cols = 28, 28
-# number of convolutional filters to use
-n_filters = 32
-# size of pooling area for max pooling
-pool_size = (2, 2)
-# convolution kernel size
-kernel_size = (3, 3)
 
 
 def setup_data():
     # the data, shuffled and split between train and test sets
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    if K.image_dim_ordering() == 'th':
-        X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-        X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
         input_shape = (1, img_rows, img_cols)
     else:
-        X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-        X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
         input_shape = (img_rows, img_cols, 1)
 
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train /= 255
-    X_test /= 255
-    print('X_train shape:', X_train.shape)
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
 
     # convert class vectors to binary class matrices
-    Y_train = np_utils.to_categorical(y_train, nb_classes)
-    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
-    return input_shape, (X_train, Y_train), (X_test, Y_test)
+    return input_shape, (x_train, y_train), (x_test, y_test)
 
 
 def build_model(input_shape):
     model = Sequential()
 
-    model.add(Conv2D(n_filters, kernel_size=kernel_size,
-                     padding='valid',
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
                      input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(Conv2D(n_filters, kernel_size=kernel_size))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Conv2D(64, kernel_size=(3, 3),
+                     activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
+    model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(nb_classes))
-    model.add(Activation('softmax'))
+    model.add(Dense(num_classes, activation='softmax'))
 
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adadelta',
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
                   metrics=['accuracy'])
     return model
 
@@ -136,10 +126,9 @@ model = build_model(input_shape)
 model.fit_generator(
     training_streamer.tuples('X', 'y', cycle=True),
     steps_per_epoch=steps_per_epoch,
-    epochs=num_epochs,
+    epochs=epochs,
     verbose=1,
-    validation_data=(X_test, Y_test),
-    validation_steps=100)
+    validation_data=(X_test, Y_test))
 
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
