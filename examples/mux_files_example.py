33dd495eef18ec29fea4db817d1aca00c381ee39
Christopher Jacoby <christopher.jacoby@gmail.com>
2017-03-18 12:52:16 -0700

    removed some unnecessary cruft

--- examples/mux_files_example.py
+++ examples/mux_files_example.py
@@ -15,19 +15,9 @@ MNIST training set evenly into three pieces, and save them to thier own
 
 from __future__ import print_function
 import numpy as np
-import keras
 from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-from sklearn.model_selection import ShuffleSplit
 
 import pescador
-
-batch_size = 128
-num_classes = 10
-epochs = 12
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -45,28 +35,7 @@ def load_mnist():
     # the data, shuffled and split between train and test sets
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    if K.image_data_format() == 'channels_first':
-        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-        input_shape = (1, img_rows, img_cols)
-    else:
-        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-        input_shape = (img_rows, img_cols, 1)
-
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
-
-    # convert class vectors to binary class matrices
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
-
-    return input_shape, (x_train, y_train), (x_test, y_test)
+    return (x_train, y_train), (x_test, y_test)
 
 
 def split_and_save_datasets(X, Y, paths):
@@ -83,7 +52,7 @@ def split_and_save_datasets(X, Y, paths):
         np.savez(paths[i], X=X_i, Y=Y_i)
 
 
-input_shape, (X_train, Y_train), (X_test, Y_test) = load_mnist()
+(X_train, Y_train), (X_test, Y_test) = load_mnist()
 split_and_save_datasets(X_train, Y_train, datasets)
 
 ##############################################
