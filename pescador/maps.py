552f8c02bfa838380967e5eea034a2f91db7825a
Eric Humphrey <ejhumphrey@spotify.com>
2017-04-11 22:12:13 -0400

    Added keras_tuples to maps

--- pescador/maps.py
+++ pescador/maps.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 '''Map functions'''
 import numpy as np
+import six
 
 from .exceptions import PescadorError
 from . import util
@@ -63,3 +64,57 @@ def buffer_stream(stream, buffer_size, partial=False,
 
     if data and partial:
         yield __stack_data(data)
+
+
+def keras_tuples(stream, inputs=None, outputs=None):
+    """Reformat data objects as keras-compatible tuples.
+
+    For more detail: https://keras.io/models/model/#fit
+
+    Parameters
+    ----------
+    stream : iterable
+        Stream of data objects.
+
+    inputs : string or iterable of strings, None
+        Keys to use for ordered input data.
+        If not specified, returns `None` in its place.
+
+    outputs : string or iterable of strings, default=None
+        Keys to use for ordered output data.
+        If not specified, returns `None` in its place.
+
+    Yields
+    ------
+    x : np.ndarray, list of np.ndarray, or None
+        If `inputs` is a string, `x` is a single np.ndarray.
+        If `inputs` is an iterable of strings, `x` is a list of np.ndarrays.
+        If `inputs` is a null type, `x` is None.
+    y : np.ndarray, list of np.ndarray, or None
+        If `outputs` is a string, `y` is a single np.ndarray.
+        If `outputs` is an iterable of strings, `y` is a list of np.ndarrays.
+        If `outputs` is a null type, `y` is None.
+    """
+    if inputs and isinstance(inputs, six.string_types):
+        inputs = [inputs]
+    if outputs and isinstance(outputs, six.string_types):
+        outputs = [outputs]
+
+    inputs, outputs = (inputs or []), (outputs or [])
+    if not inputs + outputs:
+        raise PescadorError('At least one key must be given for '
+                            '`inputs` or `outputs`')
+
+    for data in stream:
+        try:
+            x = list(data[key] for key in inputs) or None
+            if len(inputs) == 1:
+                x = x[0]
+
+            y = list(data[key] for key in outputs) or None
+            if len(outputs) == 1:
+                y = y[0]
+
+            yield (x, y)
+        except TypeError:
+            raise PescadorError("Malformed data stream: {}".format(data))
--- tests/test_maps.py
+++ tests/test_maps.py
@@ -31,3 +31,28 @@ def test_buffer_stream():
     assert len(outputs) == len(expected)
     for exp, obs in zip(expected, outputs):
         T.__eq_batch(exp, obs)
+
+
+def test_keras_tuples():
+    data = [{"foo": np.array([n]), "bar": np.array([n / 2.]),
+             "whiz": np.array([2 * n]), "bang": np.array([n ** 2])}
+            for n in range(10)]
+
+    stream = pescador.maps.keras_tuples(data, inputs="foo", outputs="bar")
+    for n, (x, y) in enumerate(stream):
+        assert n == x == y * 2
+
+    stream = pescador.maps.keras_tuples(data, outputs="whiz")
+    for n, (x, y) in enumerate(stream):
+        assert n == y / 2.
+        assert x is None
+
+    stream = pescador.maps.keras_tuples(data, inputs="bang")
+    for n, (x, y) in enumerate(stream):
+        assert n == x ** 0.5
+        assert y is None
+
+    stream = pescador.maps.keras_tuples(data, inputs=["foo", "bang"],
+                                        outputs=["bar", "whiz"])
+    for n, (x, y) in enumerate(stream):
+        assert len(x) == len(y) == 2
