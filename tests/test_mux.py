af330c369e95608991bf1c8e28f2c269dd55bf59
Christopher Jacoby <christopher.jacoby@gmail.com>
2017-10-09 11:37:57 -0400

    completed rebase into stale branch and cleaned up.

--- tests/test_mux.py
+++ tests/test_mux.py
@@ -488,10 +488,6 @@ class TestPoissonMux_SingleActive:
              "PoissonMux"])
     def test_critical_mux_of_rate_limited_muxes(self, mux_class):
         # Check on Issue #79
-        def _choice(vals):
-            while True:
-                yield random.choice(vals)
-
         ab = pescador.Streamer(_choice, 'ab')
         cd = pescador.Streamer(_choice, 'cd')
         ef = pescador.Streamer(_choice, 'ef')
@@ -538,41 +534,43 @@ class TestPoissonMux_SingleActive:
 
         assert len(list(mux(max_iter=100))) == 0
 
+    @pytest.mark.parametrize('mux_class', [
+        functools.partial(pescador.mux.Mux,
+                          with_replacement=False, revive=True),
+        functools.partial(pescador.mux.PoissonMux, mode="single_active")],
+        ids=["DeprecatedMux",
+             "PoissonMux"])
+    def test_mux_stacked_uniform_convergence(self, mux_class):
+        """This test is designed to check that boostrapped streams of data
+        (Streamer subsampling, rate limiting) cascaded through multiple
+        multiplexors converges in expectation to a flat, uniform sample of the
+        stream directly.
+        """
+        ab = pescador.Streamer(_choice, 'ab')
+        cd = pescador.Streamer(_choice, 'cd')
+        ef = pescador.Streamer(_choice, 'ef')
+        mux1 = mux_class([ab, cd, ef], k=2, rate=2, random_state=1357)
 
-def test_mux_stacked_uniform_convergence():
-    """This test is designed to check that boostrapped streams of data
-    (Streamer subsampling, rate limiting) cascaded through multiple
-    multiplexors converges in expectation to a flat, uniform sample of the
-    stream directly.
-    """
-    ab = pescador.Streamer(_choice, 'ab')
-    cd = pescador.Streamer(_choice, 'cd')
-    ef = pescador.Streamer(_choice, 'ef')
-    mux1 = pescador.Mux([ab, cd, ef], k=2, rate=2, with_replacement=False,
-                        revive=True, random_state=1357)
+        gh = pescador.Streamer(_choice, 'gh')
+        ij = pescador.Streamer(_choice, 'ij')
+        kl = pescador.Streamer(_choice, 'kl')
 
-    gh = pescador.Streamer(_choice, 'gh')
-    ij = pescador.Streamer(_choice, 'ij')
-    kl = pescador.Streamer(_choice, 'kl')
+        mux2 = mux_class([gh, ij, kl], k=2, rate=2, random_state=2468)
 
-    mux2 = pescador.Mux([gh, ij, kl], k=2, rate=2, with_replacement=False,
-                        revive=True, random_state=2468)
+        stacked_mux = mux_class([mux1, mux2], k=2, rate=None,
+                                random_state=12345)
 
-    stacked_mux = pescador.Mux([mux1, mux2], k=2, rate=None,
-                               with_replacement=False, revive=True,
-                               random_state=12345)
+        max_iter = 1000
+        chars = 'abcdefghijkl'
+        samples = list(stacked_mux.iterate(max_iter=max_iter))
+        counter = collections.Counter(samples)
+        assert set(chars) == set(counter.keys())
 
-    max_iter = 1000
-    chars = 'abcdefghijkl'
-    samples = list(stacked_mux.iterate(max_iter=max_iter))
-    counter = collections.Counter(samples)
-    assert set(chars) == set(counter.keys())
+        counts = np.asarray(list(counter.values()))
 
-    counts = np.asarray(list(counter.values()))
-
-    # Check that the pvalue for the chi^2 test is at least 0.95
-    test = scipy.stats.chisquare(counts)
-    assert test.pvalue >= 0.95
+        # Check that the pvalue for the chi^2 test is at least 0.95
+        test = scipy.stats.chisquare(counts)
+        assert test.pvalue >= 0.95
 
 
 class TestRoundRobinMux:
